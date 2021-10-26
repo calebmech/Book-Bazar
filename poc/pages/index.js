5f98aa63b9ef059224d859389d1d3b3e6c21c740
David Thompson <davidethompson@me.com>
2021-10-25 23:44:06 -0400

    POC frontend for "create posting" workflow
    This wizard demonstrates the workflow of creating a posting for selling
    a used textbook.

---
+++ poc/pages/index.js
@@ -0,0 +1,49 @@
+import Head from 'next/head';
+import CreatePostingWizard from '../src/createPostingWizard.tsx';
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>Book Bazaar</title>
+      </Head>
+
+      <main>
+        <content>
+          <h1>
+            Create Used Book Posting
+          </h1>
+
+          <CreatePostingWizard />
+        </content>
+      </main>
+
+      <style jsx global>{`
+        html,
+        body {
+          padding: 0;
+          margin: 0;
+          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+            sans-serif;
+        }
+
+        main {
+          width: 500px;
+          margin: 0 auto;
+          padding: 18px;
+        }
+
+        p {
+          text-align: justify;
+        }
+
+        video {
+          width: 500px;
+          border: 5px black solid;
+          border-radius: 5px;
+        }
+      `}</style>
+    </div>
+  )
+}
