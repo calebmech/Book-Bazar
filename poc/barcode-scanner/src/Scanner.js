9d6e062bffeb9eb8bb08a5dd8797d673ed5e4f6c
Matthew Williams <matthew.rc3@gmail.com>
2021-11-01 12:10:59 -0400

    Initial commit for barcode scanning proof of concept. Currently have only been able to get it working with static images that are very clear.

---
+++ poc/barcode-scanner/src/Scanner.js
@@ -0,0 +1,96 @@
+import React, { useEffect } from "react";
+import Quagga from "quagga";
+
+const Scanner = props => {
+  const { onDetected } = props;
+
+  useEffect(() => {
+    Quagga.init({
+        "inputStream": {
+          "type": "LiveStream",
+          "constraints": {
+            "width": { "min": 450 },
+            "height": { "min": 300 },
+            "facingMode": "environment",
+            "aspectRatio": { "min": 1, "max": 2 }
+          }
+        },
+        "locator": {
+          "patchSize": "medium",
+          "halfSample": true
+        },
+        "numOfWorkers": 2,
+        "frequency": 10,
+        "decoder": {
+          "readers": ["ean_reader"]
+        },
+        "locate": true
+      }, err => {
+      if (err) {
+        console.log(err, "error msg");
+      }
+      Quagga.start();
+      return () => {
+        Quagga.stop()
+      }
+    });
+
+    //detecting boxes on stream
+    Quagga.onProcessed(result => {
+      var drawingCtx = Quagga.canvas.ctx.overlay,
+        drawingCanvas = Quagga.canvas.dom.overlay;
+
+      if (result) {
+        if (result.boxes) {
+          drawingCtx.clearRect(
+            0,
+            0,
+            Number(drawingCanvas.getAttribute("width")),
+            Number(drawingCanvas.getAttribute("height"))
+          );
+          result.boxes
+            .filter(function(box) {
+              return box !== result.box;
+            })
+            .forEach(function(box) {
+              Quagga.ImageDebug.drawPath(box, { x: 0, y: 1 }, drawingCtx, {
+                color: "green",
+                lineWidth: 2
+              });
+            });
+        }
+
+        if (result.box) {
+          Quagga.ImageDebug.drawPath(result.box, { x: 0, y: 1 }, drawingCtx, {
+            color: "#00F",
+            lineWidth: 2
+          });
+        }
+
+        if (result.codeResult && result.codeResult.code) {
+          Quagga.ImageDebug.drawPath(
+            result.line,
+            { x: "x", y: "y" },
+            drawingCtx,
+            { color: "red", lineWidth: 3 }
+          );
+        }
+      }
+    });
+    const detected = result => {
+    onDetected(result.codeResult.code);
+  };
+
+    Quagga.onDetected(detected);
+  }, [onDetected]);
+
+
+  return (
+    // If you do not specify a target,
+    // QuaggaJS would look for an element that matches
+    // the CSS selector #interactive.viewport
+    <div id="interactive" className="viewport" />
+  );
+};
+
+export default Scanner;
