da72f4e17dc9b4a9271556b3915071e35cdb7690
Matthew Williams <matthew.rc3@gmail.com>
2021-11-19 13:18:21 -0500

    Created api routes for course/:id/books and course/:id/posts

---
+++ pages/api/course/[id]/posts.ts
@@ -0,0 +1,36 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { Prisma, PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+async function getPostsForCourse(id: string) {
+  const course = await prisma.course.findFirst({
+    where: {
+      id: id,
+    },
+    include: {
+      books: {
+        include: {
+          posts: true
+        },
+      },
+    },
+  });
+
+  return course ? course.books.map(book => book.posts).flat() : null;
+}
+
+export type Posts = Prisma.PromiseReturnType<typeof getPostsForCourse>;
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse<Posts>) {
+  const { id } = req.query;
+  const courseID = id as string;
+
+  const course = await getPostsForCourse(courseID);
+  if (course) {
+    res.status(200).json(course);
+  }
+  else {
+    res.status(404).json(null);
+  }
+}
