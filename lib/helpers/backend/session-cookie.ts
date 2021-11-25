fd7542fe708a9cd99895541b6b315bb166e30490
Caleb Mech <cmech1@gmail.com>
2021-11-25 12:08:19 -0500

    Change VERCEL_URL to BASE_URL

--- lib/helpers/backend/session-cookie.ts
+++ lib/helpers/backend/session-cookie.ts
@@ -2,22 +2,25 @@ import cookie from 'cookie';
 
 export const SESSION_TOKEN_COOKIE = 'session-token';
 
-export function createSessionCookie(sessionToken: string, expirationDate: Date): string {
-  // Remove port from URL (e.g. localhost:4000 -> localhost)
-  if (!process.env.VERCEL_URL) {
-    throw new Error('process.env.VERCEL_URL must be set.');
-  }
-  const { hostname } = new URL('https://' + process.env.VERCEL_URL);
+export function createSessionCookie(
+    sessionToken: string,
+    expirationDate: Date
+): string {
+    // Remove port from URL (e.g. localhost:4000 -> localhost)
+    if (!process.env.BASE_URL) {
+        throw new Error('process.env.BASE_URL must be set.');
+    }
+    const { hostname } = new URL('https://' + process.env.BASE_URL);
 
-  return cookie.serialize(SESSION_TOKEN_COOKIE, sessionToken, {
-    domain: hostname,
-    expires: expirationDate,
-    secure: true,
-    httpOnly: true,
-    path: '/',
-    sameSite: 'lax',
-  });
+    return cookie.serialize(SESSION_TOKEN_COOKIE, sessionToken, {
+        domain: hostname,
+        expires: expirationDate,
+        secure: true,
+        httpOnly: true,
+        path: '/',
+        sameSite: 'lax',
+    });
 }
 export function createDeleteSessionCookie(): string {
-  return createSessionCookie('deleted', new Date(0));
+    return createSessionCookie('deleted', new Date(0));
 }
--- lib/services/magic.ts
+++ lib/services/magic.ts
@@ -5,65 +5,71 @@ import { createSession } from './session';
 import { findOrCreateUser } from './user';
 
 if (process.env.SENDGRID_API_KEY) {
-  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 } else {
-  throw new Error('process.env.SENDGRID_API_KEY must be set.');
+    throw new Error('process.env.SENDGRID_API_KEY must be set.');
 }
 
 function getMagicLinkExpirationDate(): Date {
-  return new Date(Date.now() + 1000 * 60 * 60); // 1 hour from now
+    return new Date(Date.now() + 1000 * 60 * 60); // 1 hour from now
 }
 
 export async function sendMagicLink(email: string): Promise<void> {
-  const token = createToken();
-  const hashedToken = hashToken(token);
+    const token = createToken();
+    const hashedToken = hashToken(token);
 
-  await prisma.verificationEmail.create({
-    data: {
-      email,
-      hashedToken,
-      expirationDate: getMagicLinkExpirationDate(),
-    },
-  });
+    await prisma.verificationEmail.create({
+        data: {
+            email,
+            hashedToken,
+            expirationDate: getMagicLinkExpirationDate(),
+        },
+    });
 
-  if (!process.env.VERCEL_URL) {
-    throw new Error('process.env.VERCEL_URL must be set.');
-  }
-  const magicLink = 'https://' + process.env.VERCEL_URL + '/magic?token=' + encodeURIComponent(token);
+    if (!process.env.BASE_URL) {
+        throw new Error('process.env.BASE_URL must be set.');
+    }
+    const magicLink =
+        'https://' +
+        process.env.BASE_URL +
+        '/magic?token=' +
+        encodeURIComponent(token);
 
-  if (!process.env.SENDGRID_EMAIL_FROM) {
-    throw new Error('process.env.SENDGRID_EMAIL_FROM must be set.');
-  }
-  await sgMail.send({
-    from: process.env.SENDGRID_EMAIL_FROM,
-    to: email,
-    subject: 'Login to Book Bazar',
-    html: `<a href="${magicLink}">Click here</a> to login`,
-  });
+    if (!process.env.SENDGRID_EMAIL_FROM) {
+        throw new Error('process.env.SENDGRID_EMAIL_FROM must be set.');
+    }
+    await sgMail.send({
+        from: process.env.SENDGRID_EMAIL_FROM,
+        to: email,
+        subject: 'Login to Book Bazar',
+        html: `<a href="${magicLink}">Click here</a> to login`,
+    });
 }
 
 export interface TokenWithExpiration {
-  token: string;
-  expirationDate: Date;
+    token: string;
+    expirationDate: Date;
 }
 
-export async function consumeMagicLink(verificationToken: string): Promise<TokenWithExpiration | null> {
-  // Only allow verification email to be used once
-  const verificationEmail = await prisma.verificationEmail.delete({
-    where: { hashedToken: hashToken(verificationToken) },
-  });
+export async function consumeMagicLink(
+    verificationToken: string
+): Promise<TokenWithExpiration | null> {
+    // Only allow verification email to be used once
+    const verificationEmail = await prisma.verificationEmail.delete({
+        where: { hashedToken: hashToken(verificationToken) },
+    });
 
-  if (!verificationEmail || verificationEmail.expirationDate < new Date()) {
-    return null;
-  }
+    if (!verificationEmail || verificationEmail.expirationDate < new Date()) {
+        return null;
+    }
 
-  const user = await findOrCreateUser(verificationEmail.email);
+    const user = await findOrCreateUser(verificationEmail.email);
 
-  const sessionToken = createToken();
-  const session = await createSession(sessionToken, user.id);
+    const sessionToken = createToken();
+    const session = await createSession(sessionToken, user.id);
 
-  return {
-    token: sessionToken,
-    expirationDate: session.expirationDate,
-  };
+    return {
+        token: sessionToken,
+        expirationDate: session.expirationDate,
+    };
 }
