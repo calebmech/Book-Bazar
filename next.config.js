7d43f4ca563564793419a0e7cb2cef27507a3b03
Caleb Mech <cmech1@gmail.com>
2021-11-28 20:50:34 -0500

    Added Swagger UI to Next.js App

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  async rewrites() {
+    return {
+      afterFiles: [
+        {
+          source: "/api/swagger",
+          destination: "/api/swagger/index.html",
+        },
+      ],
+    };
+  },
 };
