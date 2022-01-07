0d49cbbb8b001c4420d586403c65bb3a9e893eab
Matthew Williams <matthew.rc3@gmail.com>
2022-01-07 16:18:51 -0500

    Created course service module

---
+++ lib/services/course.ts
@@ -0,0 +1,44 @@
+import type { Prisma } from "@prisma/client";
+import { prisma } from "@lib/services/db";
+
+export type CourseWithBooks = Prisma.PromiseReturnType<typeof getCourseWithBooks>;
+
+export async function getCourseWithBooks(id: string)  {
+  return prisma.course.findUnique({
+    where: {
+      id: id,
+    },
+    include: {
+      books: true,
+    }
+  });
+}
+
+export async function getPostsForCourse(id : string, length: number, page: number, includeUser: boolean) {
+  const course = await getCourseWithBooksWithPosts(id, includeUser);
+  if (course) {
+    const start: number = page*length;
+    const end: number = (page+1)*length;
+    return course.books.map(book => book.posts).flat().slice(start, end);
+  }
+  else return null;
+}
+
+async function getCourseWithBooksWithPosts(id: string, includeUser: boolean) {
+  return prisma.course.findUnique({
+    where: {
+      id: id,
+    },
+    include: {
+      books: {
+        include: {
+          posts: {
+            include: {
+              user: includeUser
+            }
+          }
+        },
+      },
+    },
+  });
+} 
