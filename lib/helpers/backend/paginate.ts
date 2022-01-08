b6cb775a9582356dc5a384086de245ca2390df5d
Matthew Williams <matthew.rc3@gmail.com>
2022-01-08 17:22:59 -0500

    Use prisma call to paginate

--- lib/helpers/backend/paginate.ts
+++
@@ -1,5 +0,0 @@
-export function paginateResults(array: any[], length: number, page: number) {
-  const start: number = page*length;
-  const end: number = (page+1)*length;
-  return array.slice(start, end);
-}
--- lib/services/course.ts
+++ lib/services/course.ts
@@ -1,6 +1,5 @@
 import type { Prisma } from "@prisma/client";
 import { prisma } from "@lib/services/db";
-import { paginateResults } from "@lib/helpers/backend/paginate";
 
 export type CourseWithBooks = Prisma.PromiseReturnType<typeof getCourseWithBooks>;
 
@@ -31,28 +30,19 @@ export async function getCourseWithBooks(id: string)  {
  * @returns an array of posts from the course with the given id that is paginated, or null if the course with the given id cannot be found
  */
 export async function getPostsForCourse(id : string, length: number, page: number, includeUser: boolean) {
-  const course = await getCourseWithBooksWithPosts(id, includeUser);
-  if (course) {
-    const postArray = course.books.map(book => book.posts).flat();
-    return paginateResults(postArray, length, page);
-  }
-  else return null;
-}
-
-async function getCourseWithBooksWithPosts(id: string, includeUser: boolean) {
-  return prisma.course.findUnique({
-    where: {
-      id: id,
-    },
+  return prisma.post.findMany({
+    skip: page * length,
+    take: length,
     include: {
-      books: {
-        include: {
-          posts: {
-            include: {
-              book: true,
-              user: includeUser
-            }
-          }
+      book: true,
+      user: includeUser,
+    },
+    where: {
+      book: {
+        courses: {
+          some: {
+            id,
+          },
         },
       },
     },
