e8da1bf5459954f42d915a8cc4d3831482db0f1a
Caleb Mech <cmech1@gmail.com>
2022-01-20 11:49:29 -0500

    Added user delete cascade to posts

--- prisma/migrations/20220120163235_user_delete_cascades/migration.sql
+++ prisma/migrations/20220120163235_user_delete_cascades/migration.sql
@@ -1,5 +1,11 @@
 -- DropForeignKey
+ALTER TABLE "Post" DROP CONSTRAINT "Post_userId_fkey";
+
+-- DropForeignKey
 ALTER TABLE "Session" DROP CONSTRAINT "Session_userId_fkey";
+
+-- AddForeignKey
+ALTER TABLE "Post" ADD CONSTRAINT "Post_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Session" ADD CONSTRAINT "Session_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
