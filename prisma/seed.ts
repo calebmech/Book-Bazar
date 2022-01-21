63651c0549c714a0f1222a2daf1fbbbfe4640e0d
Caleb Mech <cmech1@gmail.com>
2022-01-20 21:42:59 -0500

    Don't seed database for E2E tests

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,3 +1,4 @@
+import { IS_E2E } from "../lib/helpers/env";
 import { PrismaClient } from "@prisma/client";
 import fs from "fs/promises";
 import path from "path";
@@ -6,6 +7,10 @@ import { updateDatabase } from "../bookstore-scrapper/database/updateDatabase";
 const prisma = new PrismaClient();
 
 async function main() {
+  if (IS_E2E) {
+    return;
+  }
+
   const rawData = await fs.readFile(
     path.join("bookstore-scrapper/data.json"),
     "utf-8"
