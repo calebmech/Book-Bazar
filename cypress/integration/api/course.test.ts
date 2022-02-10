0ed0cd11c1d04cf2a4366010fbe22f9466cac8cb
Matthew Williams <44421339+Matthew-dw@users.noreply.github.com>
2022-02-10 14:32:06 -0500

    Update course endpoint to use course code in url (#122)
    * Update course endpoint to use course code in url
    
    * remove console log
    
    Co-authored-by: Caleb Mech <cmech1@gmail.com>

--- cypress/integration/api/course.test.ts
+++ cypress/integration/api/course.test.ts
@@ -1,9 +1,11 @@
 import { HttpMethod } from "@lib/http-method";
 import { StatusCodes } from "http-status-codes";
-import { v4 } from "uuid";
 import {
-  TEST_COURSE_UUID
+  TEST_COURSE,
+  TEST_DEPARTMENT,
 } from "../../support/constants";
+
+const TEST_COURSE_QUERY = TEST_DEPARTMENT.abbreviation + "-" + TEST_COURSE.code;
 
 describe("test course api", () => {
   beforeEach(() => {
@@ -17,20 +19,19 @@ describe("test course api", () => {
   it("should give a 200 when GETting an existing course", () => {
     cy.request({
       method: HttpMethod.GET,
-      url: `/api/course/${TEST_COURSE_UUID}`,
+      url: `/api/course/${TEST_COURSE_QUERY}`,
     }).then((response) => {
       expect(response.status).to.equal(StatusCodes.OK);
-      expect(response.body.name).to.equal("Very Hard Course");
-      expect(response.body.code).to.equal("2H03");
-      expect(response.body.term).to.equal("Winter");
+      expect(response.body.name).to.equal(TEST_COURSE.name);
+      expect(response.body.code).to.equal(TEST_COURSE.code);
+      expect(response.body.term).to.equal(TEST_COURSE.term);
     });
   });
 
-  
   it("should give a 200 when GETting posts for an existing course", () => {
     cy.request({
       method: HttpMethod.GET,
-      url: `/api/course/${TEST_COURSE_UUID}/posts`,
+      url: `/api/course/${TEST_COURSE_QUERY}/posts`,
     }).then((response) => {
       expect(response.status).to.equal(StatusCodes.OK);
       expect(response.body.length).to.equal(4);
@@ -45,21 +46,21 @@ describe("test course api", () => {
     cy.login();
     cy.request({
       method: HttpMethod.GET,
-      url: `/api/course/${TEST_COURSE_UUID}/posts`,
+      url: `/api/course/${TEST_COURSE_QUERY}/posts`,
     }).then((response) => {
       expect(response.status).to.equal(StatusCodes.OK);
       expect(response.body.length).to.equal(4);
-      expect(response.body[0].user.name).to.be.oneOf(["Test User", "Other"])
-      expect(response.body[1].user.name).to.be.oneOf(["Test User", "Other"])
-      expect(response.body[2].user.name).to.be.oneOf(["Test User", "Other"])
-      expect(response.body[3].user.name).to.be.oneOf(["Test User", "Other"])
+      expect(response.body[0].user.name).to.be.oneOf(["Test User", "Other"]);
+      expect(response.body[1].user.name).to.be.oneOf(["Test User", "Other"]);
+      expect(response.body[2].user.name).to.be.oneOf(["Test User", "Other"]);
+      expect(response.body[3].user.name).to.be.oneOf(["Test User", "Other"]);
     });
   });
 
   it("should include first 3 of 4 posts when GETting posts for an existing course with a length of 3", () => {
     cy.request({
       method: HttpMethod.GET,
-      url: `/api/course/${TEST_COURSE_UUID}/posts?length=3&page=0`,
+      url: `/api/course/${TEST_COURSE_QUERY}/posts?length=3&page=0`,
     }).then((response) => {
       expect(response.status).to.equal(StatusCodes.OK);
       expect(response.body.length).to.equal(3);
@@ -69,7 +70,7 @@ describe("test course api", () => {
   it("should include last post of 4 when GETting posts for an existing course with a length of 3 and page of 1", () => {
     cy.request({
       method: HttpMethod.GET,
-      url: `/api/course/${TEST_COURSE_UUID}/posts?length=3&page=1`,
+      url: `/api/course/${TEST_COURSE_QUERY}/posts?length=3&page=1`,
     }).then((response) => {
       expect(response.status).to.equal(StatusCodes.OK);
       expect(response.body.length).to.equal(1);
@@ -79,14 +80,14 @@ describe("test course api", () => {
   it("should include zero posts when GETting posts for an existing course with a length of 3 and page of 2", () => {
     cy.request({
       method: HttpMethod.GET,
-      url: `/api/course/${TEST_COURSE_UUID}/posts?length=3&page=2`,
+      url: `/api/course/${TEST_COURSE_QUERY}/posts?length=3&page=2`,
     }).then((response) => {
       expect(response.status).to.equal(StatusCodes.OK);
       expect(response.body.length).to.equal(0);
     });
   });
-  
-  it("should give a 400 when trying to GET a course with an invalid uuid", () => {
+
+  it("should give a 400 when trying to GET a course with an invalid code", () => {
     cy.request({
       method: HttpMethod.GET,
       url: `/api/course/hello`,
@@ -96,7 +97,7 @@ describe("test course api", () => {
     });
   });
 
-  it("should give a 400 when trying to GET posts for a course with an invalid uuid", () => {
+  it("should give a 400 when trying to GET posts for a course with an invalid code", () => {
     cy.request({
       method: HttpMethod.GET,
       url: `/api/course/hello/posts`,
@@ -109,7 +110,7 @@ describe("test course api", () => {
   it("should give a 404 when trying to GET a course that doesn't exist", () => {
     cy.request({
       method: HttpMethod.GET,
-      url: `/api/course/${v4()}`,
+      url: `/api/course/swagger-3S03`,
       failOnStatusCode: false,
     }).then((response) => {
       expect(response.status).to.equal(StatusCodes.NOT_FOUND);
@@ -119,10 +120,10 @@ describe("test course api", () => {
   it("should give a 404 when trying to GET posts for a course with an invalid uuid", () => {
     cy.request({
       method: HttpMethod.GET,
-      url: `/api/course/${v4()}/posts`,
+      url: `/api/course/swagger-3S03/posts`,
       failOnStatusCode: false,
     }).then((response) => {
-      console.log(response.body)
+      console.log(response.body);
       expect(response.status).to.equal(StatusCodes.NOT_FOUND);
     });
   });
