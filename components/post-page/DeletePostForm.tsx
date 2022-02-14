4006a1bafe5aaff9aa54cb8f52316ceb3be5a4fa
Matthew Williams <44421339+Matthew-dw@users.noreply.github.com>
2022-02-14 13:31:31 -0500

    74 post page (#121)
    * Starting post page
    
    * Add functionality to buttons
    
    * get image from google books if not with post
    
    * Small adjustments
    
    * remove use of moment, replacing with helper func
    
    * Add deep link, fixing some styling
    
    * fix logged out error
    
    * switch to next image

---
+++ components/post-page/DeletePostForm.tsx
@@ -0,0 +1,75 @@
+import {
+  AlertDialog,
+  AlertDialogBody,
+  AlertDialogContent,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogOverlay,
+  Button,
+  Icon,
+  useDisclosure,
+} from "@chakra-ui/react";
+import { TrashIcon } from "@heroicons/react/outline";
+import { useDeletePostMutation } from "@lib/hooks/post";
+import { Post } from "@prisma/client";
+import { useRef } from "react";
+
+export default function DeletePostForm({ post }: { post: Post }) {
+  const { isOpen, onClose, onOpen } = useDisclosure();
+  const cancelRef = useRef(null);
+
+  const mutation = useDeletePostMutation(post.id);
+
+  const handleDeleteSubmit = () => {
+    mutation.mutate();
+  };
+
+  return (
+    <>
+      <Button
+        onClick={() => onOpen()}
+        colorScheme="red"
+        leftIcon={<Icon as={TrashIcon} />}
+      >
+        Delete post
+      </Button>
+      <AlertDialog
+        isOpen={isOpen}
+        leastDestructiveRef={cancelRef}
+        onClose={onClose}
+      >
+        <AlertDialogOverlay>
+          <AlertDialogContent>
+            <AlertDialogHeader fontSize="lg" fontWeight="bold">
+              Delete Post
+            </AlertDialogHeader>
+
+            <AlertDialogBody>
+              {!mutation.error
+                ? "Are you sure you want to delete this post?"
+                : "An error occurred, please try again."}
+            </AlertDialogBody>
+
+            <AlertDialogFooter>
+              <Button
+                ref={cancelRef}
+                onClick={() => onClose()}
+                isDisabled={mutation.isLoading}
+              >
+                Cancel
+              </Button>
+              <Button
+                colorScheme="red"
+                onClick={handleDeleteSubmit}
+                isLoading={mutation.isLoading}
+                ml={3}
+              >
+                Delete
+              </Button>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialogOverlay>
+      </AlertDialog>
+    </>
+  );
+}
