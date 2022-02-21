3e95405de7516b05c10b16de3027b590f5dfed19
Caleb Mech <cmech1@gmail.com>
2022-02-21 13:45:46 -0500

    Style tweaks to book and search page

--- pages/book/[isbn].tsx
+++ pages/book/[isbn].tsx
@@ -1,30 +1,37 @@
 import {
-  Badge,
   Box,
   Button,
+  Divider,
   Flex,
-  Grid,
+  Heading,
   HStack,
   Icon,
   Text,
+  VStack,
   Wrap,
 } from "@chakra-ui/react";
-import {
-  OfficeBuildingIcon,
-  CalendarIcon,
-  BookOpenIcon,
-  ViewBoardsIcon,
-  AcademicCapIcon,
-  ExternalLinkIcon,
-} from "@heroicons/react/solid";
-import Image from "next/image";
 import { PostCardList } from "@components/CardList";
 import Layout from "@components/Layout";
+import {
+  AcademicCapIcon,
+  BookOpenIcon,
+  CalendarIcon,
+  ExternalLinkIcon,
+  OfficeBuildingIcon,
+  ViewBoardsIcon,
+} from "@heroicons/react/solid";
+import pageTitle from "@lib/helpers/frontend/page-title";
+import {
+  resolveBookTitle,
+  resolveImageUrl,
+} from "@lib/helpers/frontend/resolve-book-data";
+import { formatIntPrice } from "@lib/helpers/priceHelpers";
 import { useBookQuery } from "@lib/hooks/book";
 import type { NextPage } from "next";
-import { useRouter } from "next/router";
+import Head from "next/head";
+import Image from "next/image";
 import Link from "next/link";
-import { resolveImageUrl } from "@lib/helpers/frontend/resolve-book-data";
+import { useRouter } from "next/router";
 
 const BookPage: NextPage = () => {
   const router = useRouter();
@@ -33,7 +40,7 @@ const BookPage: NextPage = () => {
   if (!book) {
     return null;
   }
-  const { name, googleBook, posts } = book;
+  const { googleBook, posts } = book;
 
   const postsWithBookIncluded = posts.map((post) => {
     return {
@@ -42,7 +49,7 @@ const BookPage: NextPage = () => {
     };
   });
 
-  const courseBadges: React.ReactFragment = (
+  const CourseBadges = () => (
     <Wrap>
       {book.courses.map((course, i) => (
         <Link
@@ -55,7 +62,7 @@ const BookPage: NextPage = () => {
             colorScheme="teal"
             leftIcon={<Icon as={AcademicCapIcon} />}
           >
-            {course.dept.abbreviation + "-" + course.code}
+            {course.dept.abbreviation + " " + course.code}
           </Button>
         </Link>
       ))}
@@ -63,55 +70,54 @@ const BookPage: NextPage = () => {
   );
 
   const bookInfo: React.ReactNode = (
-    <Grid
-      width="100%"
-      templateColumns={{
-        base: "256px 1fr",
-      }}
-      templateRows={{
-        base: "300px",
-      }}
-      templateAreas={{
-        base: `'image image' 'info info'`,
-        md: `'image info'`,
-      }}
+    <Flex
+      direction={{ base: "column", md: "row" }}
       gap={{
         base: 4,
         md: 8,
       }}
     >
-      <Box gridArea="image">
+      <Box>
         <Flex
           direction="row"
-          h="100%"
           justifyContent="center"
           alignItems="center"
           background="tertiaryBackground"
           borderRadius="lg"
+          overflow="hidden"
+          height={{
+            base: 56,
+            md: "100%",
+          }}
+          width={{
+            base: "100%",
+            md: 56,
+          }}
         >
-          <Box width="128px" height="100%" position="relative">
+          <Box width={32} height="100%" position="relative">
             <Image
-              alt="book-image"
+              alt=""
               src={resolveImageUrl(book)}
               layout="fill"
               objectFit="contain"
+              priority
             />
           </Box>
         </Flex>
       </Box>
 
-      <Flex gridArea="info" direction="column" justify="space-between">
+      <Flex flex="auto" direction="column">
         <Flex direction="column">
-          <Text fontSize="2xl" fontWeight="bold">
-            {name}
-          </Text>
-          <Text fontSize="lg" fontWeight="semibold" color="secondaryText">
+          <Heading as="h1" size="lg" fontWeight="500" fontFamily="title">
+            {resolveBookTitle(book)}
+          </Heading>
+          <Text color="tertiaryText" fontWeight="semibold" mb="2">
             {googleBook?.authors?.join(", ")}
           </Text>
 
-          {courseBadges}
+          <CourseBadges />
 
-          <Box mt="2">
+          <Box my="4" maxWidth="sm">
             <HStack justify="space-between" color="tertiaryText">
               <HStack direction="row">
                 <Icon as={OfficeBuildingIcon} />
@@ -122,9 +128,9 @@ const BookPage: NextPage = () => {
                 <Text> Published Date </Text>
               </HStack>
             </HStack>
-            <HStack justify="space-between" fontSize="xl" fontStyle="bold">
-              <Text>{googleBook?.publisher ?? "-"}</Text>
-              <Text>{googleBook?.publishedDate ?? "-"}</Text>
+            <HStack justify="space-between">
+              <Text>{googleBook?.publisher ?? "\u2013"}</Text>
+              <Text>{googleBook?.publishedDate ?? "\u2013"}</Text>
             </HStack>
             <HStack justify="space-between" color="tertiaryText">
               <HStack direction="row">
@@ -136,29 +142,76 @@ const BookPage: NextPage = () => {
                 <Text> Page Count </Text>
               </HStack>
             </HStack>
-            <HStack justify="space-between" fontSize="xl" fontStyle="bold">
+            <HStack justify="space-between">
               <Text>{isbn && !Array.isArray(isbn) ? isbn : ""}</Text>
-              <Text>{googleBook?.pageCount?.toString() ?? "-"}</Text>
+              <Text>{googleBook?.pageCount?.toString() ?? "\u2013"}</Text>
             </HStack>
           </Box>
         </Flex>
         {googleBook && (
           <Button
-            mt="1"
-            onClick={() => window.open(googleBook.infoLink)}
+            width={{
+              base: "100%",
+              sm: "min-content",
+            }}
+            size="sm"
+            variant="outline"
+            colorScheme="teal"
+            as="a"
+            href={googleBook.infoLink}
+            target="_blank"
             rightIcon={<Icon as={ExternalLinkIcon} />}
           >
-            View On Google Books
+            View on Google Books
           </Button>
         )}
       </Flex>
-    </Grid>
+    </Flex>
   );
 
   return (
-    <Layout extendedHeader={bookInfo}>
-      <PostCardList posts={postsWithBookIncluded} isLinkActive={true} />
-    </Layout>
+    <>
+      <Head>
+        <title>{pageTitle(resolveBookTitle(book))}</title>
+      </Head>
+      <Layout extendedHeader={bookInfo}>
+        <PostCardList posts={postsWithBookIncluded} isLinkActive={true} />
+
+        {book.isCampusStoreBook && (
+          <>
+            <Divider
+              mx="auto"
+              my={{
+                base: "12",
+                md: "14",
+              }}
+              maxWidth="lg"
+            />
+
+            <VStack
+              mb={{
+                base: "14",
+                md: "16",
+              }}
+              spacing="5"
+            >
+              <Text>Can&apos;t find your book being sold?</Text>
+              <Button
+                colorScheme="teal"
+                as="a"
+                href="https://textbooks.mcmaster.ca"
+                target="_blank"
+                rightIcon={<Icon as={ExternalLinkIcon} />}
+              >
+                Buy from Campus Store
+                {book.campusStorePrice &&
+                  ` for $${formatIntPrice(book.campusStorePrice)}`}
+              </Button>
+            </VStack>
+          </>
+        )}
+      </Layout>
+    </>
   );
 };
 
--- pages/search.tsx
+++ pages/search.tsx
@@ -1,19 +1,14 @@
-import { useRouter } from "next/router";
-import type { NextPage } from "next";
-import { useAlgolia } from "@lib/hooks/algolia";
-import { Book } from "@prisma/client";
-import { CourseWithDept } from "@lib/services/course";
+import { Heading, Text } from "@chakra-ui/react";
 import { BookCardList, CourseCardList } from "@components/CardList";
-import { Flex, Heading, Text } from "@chakra-ui/react";
 import Layout from "@components/Layout";
 import Loading from "@components/Loading";
-
-enum HEADER {
-  RESULTS_AVAILABLE = "Results for ",
-  NO_RESULTS_AVAILABLE = "No results for ",
-  COURSES = "Courses",
-  BOOKS = "Books",
-}
+import pageTitle from "@lib/helpers/frontend/page-title";
+import { useAlgolia } from "@lib/hooks/algolia";
+import { CourseWithDept } from "@lib/services/course";
+import { Book } from "@prisma/client";
+import type { NextPage } from "next";
+import Head from "next/head";
+import { useRouter } from "next/router";
 
 const Search: NextPage = () => {
   const { query } = useRouter();
@@ -36,32 +31,40 @@ const Search: NextPage = () => {
     .filter((hit) => hit.type === "book")
     .map((book) => book.entry as Book);
 
-  return (
-    <Layout>
-      <Flex marginY={8} marginX={10} direction="column">
-        <Heading size="lg">
-          {courses.length > 0 || books.length > 0
-            ? `${HEADER.RESULTS_AVAILABLE}"${search}"`
-            : `${HEADER.NO_RESULTS_AVAILABLE}"${search}"`}
-        </Heading>
+  const SearchResults = () => {
+    if (courses.length === 0 && books.length === 0) {
+      return (
+        <Text mt="10" fontSize="xl" color="secondaryText">
+          No courses or books found.
+        </Text>
+      );
+    }
 
-        {courses.length > 0 ? (
+    return (
+      <>
+        {courses.length > 0 && (
           <CourseCardList courses={courses} isLinkActive={true} />
-        ) : (
-          <Text mt="10" fontSize="2xl" color="secondaryText">
-            No courses found.
-          </Text>
         )}
+        {books.length > 0 && <BookCardList books={books} isLinkActive={true} />}
+      </>
+    );
+  };
 
-        {books.length > 0 ? (
-          <BookCardList books={books} isLinkActive={true} />
-        ) : (
-          <Text mt="10" fontSize="2xl" color="secondaryText">
-            No books found.
-          </Text>
-        )}
-      </Flex>
-    </Layout>
+  return (
+    <>
+      <Head>
+        <title>{pageTitle(`Results for “${search}”`)}</title>
+      </Head>
+      <Layout
+        extendedHeader={
+          <Heading as="h1" size="lg" fontWeight="500" fontFamily="title">
+            Results for “{search}”
+          </Heading>
+        }
+      >
+        <SearchResults />
+      </Layout>
+    </>
   );
 };
 
