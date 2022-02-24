96ed3832a239894030e15de573af4b2b220f1830
Matthew Williams <44421339+Matthew-dw@users.noreply.github.com>
2022-02-24 16:57:07 -0500

    fix e2e tests

--- lib/hooks/book.ts
+++ lib/hooks/book.ts
@@ -4,16 +4,18 @@ import { UseQueryResult, useQuery } from "react-query";
 
 export function useBookQuery(
   bookIsbn: string | string[] | undefined,
-  page: number | null = 0,
-  length: number | null = 4
+  page: number | null = null,
+  length: number | null = null
 ): UseQueryResult<PopulatedBook> {
+  const url = "/api/book/" + bookIsbn;
+  const parameters = new URLSearchParams();
+  if (page) parameters.set("page", page.toString());
+  if (length) parameters.set("length", length.toString());
   return useQuery(
     "book-" + bookIsbn + "-" + page,
     () =>
       axios
-        .get<PopulatedBook>(
-          `/api/book/${bookIsbn}?page=${page}&length=${length}`
-        )
+        .get<PopulatedBook>(url + parameters.toString())
         .then((res) => res.data),
     {
       enabled: !!bookIsbn && !Array.isArray(bookIsbn),
