e829de4bd3a8ab1c1478eed3d30b43ed4a55a417
alkoasma <40670885+alkoasma@users.noreply.github.com>
2022-03-21 00:34:31 -0400

    added final touches to tests

--- cypress/integration/edit-post.test.ts
+++ cypress/integration/edit-post.test.ts
@@ -18,17 +18,34 @@ describe("Edit post", () => {
     cy.intercept(HttpMethod.PUT, "/api/post/*").as("editPost");
     cy.intercept(HttpMethod.GET, "/api/user/*").as("getUserPosts");
 
+    // Check if editing the post works
     cy.findAllByRole("button", { name: /edit post/i })
       .eq(0)
       .click();
 
-    cy.get('[id="description"]').click().type("EDITING THE DESCRIPTION");
+    cy.findByRole("dialog")
+      .get('[id="description"]')
+      .click()
+      .type("EDITING THE DESCRIPTION");
 
-    cy.get('[id="price"]')
+    cy.findByRole("dialog")
+      .get('[id="price"]')
       .click()
       .type("{backspace}{backspace}{backspace}{backspace}13");
 
-    cy.findAllByRole("button", { name: /save changes/i }).click();
+    cy.findByRole("dialog")
+      .findAllByRole("button", { name: /upload new photo/i })
+      .click();
+
+    cy.fixture("guy.jpg", null).as("image");
+    cy.get("input[type=file]").selectFile("@image", { force: true });
+    cy.findByRole("button", { name: /upload/i }).click();
+
+    cy.findByRole("dialog").should("contain", "Sold new for $40.00");
+
+    cy.findByRole("dialog")
+      .findAllByRole("button", { name: /save changes/i })
+      .click();
 
     cy.wait("@editPost").then(({ response }) => {
       expect(response?.statusCode).to.eq(StatusCodes.OK);
@@ -46,6 +63,15 @@ describe("Edit post", () => {
       .findByRole("img", { name: /book-image/i })
       .should("have.attr", "src")
       .should("not.be.empty");
+
+    // Check if closing the modal works
+    cy.findAllByRole("button", { name: /edit post/i })
+      .eq(0)
+      .click();
+
+    cy.findByRole("dialog").findAllByRole("button", { name: /close/i }).click();
+
+    cy.findByRole("dialog").should("not.exist");
   });
 
   it("should allow a user edit a post from the post page", () => {
@@ -54,17 +80,32 @@ describe("Edit post", () => {
 
     cy.intercept(HttpMethod.PUT, "/api/post/*").as("editPost");
 
+    // Check if editing the post works
     cy.findAllByRole("button", { name: /edit post/i })
       .eq(0)
       .click();
 
-    cy.get('[id="description"]').click().type("EDITING THE DESCRIPTION");
+    cy.findByRole("dialog")
+      .get('[id="description"]')
+      .click()
+      .type("EDITING THE DESCRIPTION");
 
-    cy.get('[id="price"]')
+    cy.findByRole("dialog")
+      .get('[id="price"]')
       .click()
       .type("{backspace}{backspace}{backspace}{backspace}13");
 
-    cy.findAllByRole("button", { name: /save changes/i }).click();
+    cy.findByRole("dialog")
+      .findAllByRole("button", { name: /upload new photo/i })
+      .click();
+
+    cy.fixture("guy.jpg", null).as("image");
+    cy.get("input[type=file]").selectFile("@image", { force: true });
+    cy.findByRole("button", { name: /upload/i }).click();
+
+    cy.findByRole("dialog")
+      .findAllByRole("button", { name: /save changes/i })
+      .click();
 
     cy.wait("@editPost").then(({ response }) => {
       expect(response?.statusCode).to.eq(StatusCodes.OK);
@@ -76,5 +117,14 @@ describe("Edit post", () => {
       .findAllByRole("img", { name: /post-image/i })
       .should("have.attr", "src")
       .should("not.be.empty");
+
+    // Check if close button works
+    cy.findAllByRole("button", { name: /edit post/i })
+      .eq(0)
+      .click();
+
+    cy.findByRole("dialog").findAllByRole("button", { name: /close/i }).click();
+
+    cy.findByRole("dialog").should("not.exist");
   });
 });
