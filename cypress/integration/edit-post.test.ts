60e5840092bb5301049f8c4d04e327bc59a44b74
alkoasma <40670885+alkoasma@users.noreply.github.com>
2022-03-20 23:53:10 -0400

    add edit post integration test

---
+++ cypress/integration/edit-post.test.ts
@@ -0,0 +1,87 @@
+import { HttpMethod } from "@lib/http-method";
+import { StatusCodes } from "http-status-codes";
+
+describe("Edit post", () => {
+  beforeEach(() => {
+    cy.task("db:setup");
+  });
+
+  afterEach(() => {
+    cy.task("db:teardown");
+  });
+
+  it("should allow a user edit a post from the account page", () => {
+    cy.login();
+    cy.visit("/account");
+
+    cy.intercept(HttpMethod.PUT, "/api/post/*").as("editPost");
+    cy.intercept(HttpMethod.GET, "/api/user/*").as("getUserPosts");
+
+    cy.findAllByRole("button", { name: /edit post/i })
+      .eq(0)
+      .click();
+
+    cy.get('[id="description"]').click().type("EDITING THE DESCRIPTION");
+
+    cy.get('[id="price"]')
+      .click()
+      .type("{backspace}{backspace}{backspace}{backspace}13");
+
+    cy.findAllByRole("button", { name: /save changes/i }).click();
+
+    cy.wait("@editPost").then(({ response }) => {
+      expect(response?.statusCode).to.eq(StatusCodes.OK);
+    });
+
+    cy.wait("@getUserPosts").then(({ response }) => {
+      expect(response?.statusCode).to.eq(StatusCodes.OK);
+    });
+
+    const latestPost = cy.findByRole("main").children().eq(0).children().eq(0);
+
+    latestPost.should("contain", "EDITING THE DESCRIPTION");
+    latestPost.should("contain", "$13");
+    latestPost
+      .findByRole("img", { name: /book-image/i })
+      .should("have.attr", "src")
+      .should("not.be.empty");
+  });
+
+  it("should allow a user edit a post from the post page", () => {
+    cy.login();
+    cy.visit("/account");
+
+    cy.intercept(HttpMethod.PUT, "/api/post/*").as("editPost");
+    cy.intercept(HttpMethod.GET, "/api/user/*").as("getUserPosts");
+
+    cy.wait("@getUserPosts").then(({ response }) => {
+      expect(response?.statusCode).to.eq(StatusCodes.OK);
+    });
+
+    cy.findByRole("main").children().eq(0).children().eq(0).click();
+    cy.url().should("contain", `https://${Cypress.env("BASE_URL")}/post`);
+
+    cy.findAllByRole("button", { name: /edit post/i })
+      .eq(0)
+      .click();
+
+    cy.get('[id="description"]').click().type("EDITING THE DESCRIPTION");
+
+    cy.get('[id="price"]')
+      .click()
+      .type("{backspace}{backspace}{backspace}{backspace}13");
+
+    cy.findAllByRole("button", { name: /save changes/i }).click();
+
+    cy.wait("@editPost").then(({ response }) => {
+      expect(response?.statusCode).to.eq(StatusCodes.OK);
+    });
+
+    cy.findByRole("banner").should("contain", "EDITING THE DESCRIPTION");
+    cy.findByRole("heading", { name: /algorithms/i }).should("contain", "$13");
+    cy.findByRole("banner")
+      .findAllByRole("img", { name: /post-image/i })
+      .should("have.attr", "src")
+      .should("not.be.empty");
+  });
+});
