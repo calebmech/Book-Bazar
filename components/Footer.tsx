8a3b741c1f920e95b3558dc241ead0964658491a
Caleb Mech <cmech1@gmail.com>
2022-03-30 15:30:52 -0400

    Added Github link to footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,23 +1,33 @@
-import { Box, Container } from "@chakra-ui/react";
+import { Box, Container, HStack, Link } from "@chakra-ui/react";
 import Image from "next/image";
+import GithubIcon from "./GithubIcon";
 
 export default function Footer() {
   return (
     <Box backgroundColor="secondaryBackground" padding={8} boxShadow="base">
       <Container maxWidth="container.lg">
-        <a
-          href="https://www.vercel.com?utm_source=book-bazar?utm_campaign=oss"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            src="/powered-by-vercel.svg"
-            alt="Powered by Vercel"
-            priority
-            width={212}
-            height={44}
-          />
-        </a>
+        <HStack spacing="6">
+          <Link
+            href="http://github.com/calebmech/Book-Bazar"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <GithubIcon height="40" width="40" style={{ display: "inline" }} />
+          </Link>
+          <Link
+            href="https://www.vercel.com?utm_source=book-bazar?utm_campaign=oss"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              src="/powered-by-vercel.svg"
+              alt="Powered by Vercel"
+              priority
+              width={212}
+              height={44}
+            />
+          </Link>
+        </HStack>
       </Container>
     </Box>
   );
